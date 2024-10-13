acaef9c8ad533623ea04a658ed4d5ebe29c3df35
frankMarcelino <frank.silva.ti@gmail.com>
2024-10-13 16:33:45 -0300

    atualizando dados

--- header.php
+++ header.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?></title>
 
-		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+<head>
+  <meta charset="utf-8">
+  <title><?php bloginfo('name'); ?></title>
 
-		<!-- Header Wordpress -->
-		<?php wp_head(); ?>
-		<!-- Fecha Header Wordpress -->
-	</head>
+  <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 
-	<body>
-		
-		<header>
-			<nav>
-				<ul>
-					<li class="current_page_item"><a href="/testewp/">Menu</a></li>
-					<li><a href="/testewp/sobre/">Sobre</a></li>
-					<li><a href="/testewp/contato/">Contato</a></li>
-				</ul>
-			</nav>
+  <!-- Header Wordpress -->
+  <?php wp_head(); ?>
+  <!-- Fecha Header Wordpress -->
+</head>
 
-			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+<body>
 
-			<p>Rua Marechal 29 – Copacabana – Rj</p>
-			<p class="telefone">2422-9201</p>
-		</header>
+  <header>
+    <nav>
+      <ul>
+        <li class="current_page_item"><a href="/">Menu</a></li>
+        <li><a href="/testewp/sobre/">Sobre</a></li>
+        <li><a href="/testewp/contato/">Contato</a></li>
+      </ul>
+    </nav>
+
+    <h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+
+    <p>Rua bem ali, 00 – Juazeiro do Norte – CE</p>
+    <p class="telefone">2422-9201</p>
+  </header>
